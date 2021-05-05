d73d2805ea6260ad481864fa6adf7dd8be9a58dd
Ulukman Zhamshitov <uakmatof@gmail.com>
2021-05-05 11:24:36 -0500

    first commit

---
+++ first_js_project/first.js
@@ -0,0 +1,9 @@
+
+function doAThing(){
+    let flavors = ["mint chocolate", "vanilla", "shebrt", "pistachio"];
+    if(flavors.length>3){
+        let toppings = ["hot fudge", "jelly beans","cinnamon sugar", "rainbow spranks"];
+        console.log(toppings);
+    }
+}
+doAThing();
